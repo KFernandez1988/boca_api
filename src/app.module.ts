e4ddbca7d16d7c320bc69af0c78a217d805560fd
KFernandez1988 <kfernandezgonzalez@student.fullsail.edu>
2022-12-07 08:46:04 -0400

    fixing db connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -10,6 +10,7 @@ import { BlogsModule } from './blogs/blogs.module';
 @Module({
   imports: [BlogsModule,
     SequelizeModule.forRoot({
+      uri: "postgres://zafzdvkzxpuheq:9355456622ba31e8eedb88c354bed24d4fda0fb166e67f2f5644c0de82e34ddf@ec2-3-213-66-35.compute-1.amazonaws.com:5432/d5207t21rkqj9o",
       dialect: 'postgres',
       host: 'ec2-3-213-66-35.compute-1.amazonaws.com',
       port: 5432,
